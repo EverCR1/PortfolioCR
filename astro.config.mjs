92f5525fd673e518ec5ceca90e840effa21bed24
EverCR1 <evermcr11@gmail.com>
2024-09-08 02:19:29 -0600

    Add analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import analytics from '@astrolib/analytics';
+import partytown from '@astrojs/partytown';
 
 import sitemap from "@astrojs/sitemap";
 
@@ -9,5 +11,10 @@ export default defineConfig({
   devToolbar: {
     enabled: false
   },
-  integrations: [react(), tailwind(), sitemap()]
+  integrations: [react(),
+  tailwind(), sitemap(), partytown({
+    config: {
+      forward: ["dataLayer.push"],
+    },
+  }),]
 });
